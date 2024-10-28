edae7c861a012a2cee62c8917ed9bdde6af37f83
codewithbismah <147924755+codewithbismah@users.noreply.github.com>
2024-10-28 03:10:14 -0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Providerreact from "./Providerreact";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Providerreact>{children}</Providerreact>
       </body>
     </html>
   );
